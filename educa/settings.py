8617f1e9224dd7c0ff1e17d8604cd84ed233e94f
Ramil <Warkinstar@mail.ru>
2022-10-17 13:04:27 +0600

    Serving media files

--- educa/settings.py
+++ educa/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Settings for media files
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
