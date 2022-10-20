faef5b95be00ea6d48afbc4f7ed8ed4f8c1dcb7e
Ramil <Warkinstar@mail.ru>
2022-10-20 12:23:27 +0600

    Content models (SubClasses)
    The Content model of your courses application contains a generic relation to associate different types
    of content with it. You will create a different model for each type of content. All Content models will
    have some fields in common and additional fields to store custom data. You are going to create an
    abstract model that provides the common fields for all Content models.

--- courses/models.py
+++ courses/models.py
@@ -50,10 +50,45 @@ class Content(models.Model):
         Module,
         related_name="contents",
         on_delete=models.CASCADE,
+        limit_choices_to={"model__in": ("text", "video", "image", "file")},
     )
     content_type = models.ForeignKey(
         ContentType,
-        on_delete=models.CASCADE,    
+        on_delete=models.CASCADE,
+        
     )
-    object_id = models.PositiveIntegerField
-    
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey("content_type", "object_id")
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="%(class)s_related",
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Text(ItemBase):
+    content = models.TextField()
+
+
+class File(ItemBase):
+    file = models.FileField(upload_to="files/")
+
+
+class Image(ItemBase):
+    file = models.FileField(upload_to="images/")
+
+
+class Video(ItemBase):
+    url = models.URLField()
