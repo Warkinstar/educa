f9d4a0b9022d21f77afe58a007c1f0d37295b832
Ramil <Warkinstar@mail.ru>
2022-10-31 12:10:11 +0600

    Creating a CMS
    • List the courses created by the instructor
    • Create, edit, and delete courses
    • Add modules to a course and reorder them
    • Add different types of content to each module
    • Reorder course modules and content

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Course
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
-# Create your views here.
+
+class OwnerMixin:
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
+
+
+class OwnerEditMixin:
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin, PermissionRequiredMixin):
+    model = Course
+    fields = ["subject", "title", "slug", "overview"]
+    success_url = reverse_lazy("manage_course_list")
+
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    template_name = "courses/manage/course/form.html"
+
+
+class ManageCourseListView(OwnerCourseMixin, ListView):
+    template_name = "courses/manage/course/list.html"
+    permission_required = "courses.view_course"
+
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    permission_required = "courses.add_course"
+
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    permission_required = "courses.change_course"
+
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    template_name = "courses/manage/course/delete.html"
+    permission_required = "courses.delete_course"
--- educa/urls.py
+++ educa/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("accounts.urls"))
+    path("accounts/", include("accounts.urls")),
+    path("course/", include("courses.urls")),
 ]
 
 if settings.DEBUG:
