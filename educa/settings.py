2b158d08cdd7b1437cb73311e5c9d31454659ead
Ramil <Warkinstar@mail.ru>
2023-01-05 18:56:48 +0600

    Setting up a channel layer with Redis

--- educa/settings.py
+++ educa/settings.py
@@ -223,3 +223,11 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # ASGI
 ASGI_APPLICATION = "educa.asgi.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
