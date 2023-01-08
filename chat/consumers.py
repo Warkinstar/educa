425768972ba8418470306008af91452472d4ee50
Ramil <Warkinstar@mail.ru>
2023-01-08 16:28:19 +0600

    Updating the consumer to broadcast messages

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,18 +1,42 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         # accept connection
+        self.id = self.scope["url_route"]["kwargs"]["course_id"]
+        self.room_group_name = f"chat_{self.id}"
+        # join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
 
-    # receive message from WebSocket
+    # receive message from WebSocket to group
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
+        # send message to room group
+        # self.send(text_data=json.dumps({"message": message}))
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                "type": "chat_message",  # Invoke chat_message method
+                "message": message,
+            }
+        )
+
+    # Receive message from room group
+    def chat_message(self, event):
         # send message to WebSocket
-        self.send(text_data=json.dumps({"message": message}))
+        self.send(text_data=json.dumps(event))
