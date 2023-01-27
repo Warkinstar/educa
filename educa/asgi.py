caf7508b9e59415ffeb12aaf21fbddd961a2a904
Ramil <Warkinstar@mail.ru>
2023-01-27 14:16:38 +0600

    Work websocket and channels for prod

--- educa/asgi.py
+++ educa/asgi.py
@@ -12,6 +12,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "educa.settings")
@@ -20,7 +21,9 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
         URLRouter(chat.routing.websocket_urlpatterns)
+        )
     ),
 })
