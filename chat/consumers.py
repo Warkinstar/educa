082b95c5283d05657697ba16b02574e52b39a698
Ramil <Warkinstar@mail.ru>
2023-05-07 19:54:13 +0600

    async orm requests & js
    course_chat_room.js - get_full_name;
    ChaConsumer async get_full_name,
    async_escape for save html input;
    async versions of orm requests;

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,10 +1,11 @@
 import json
 from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
-from asgiref.sync import async_to_sync
+from asgiref.sync import async_to_sync, sync_to_async
 from django.utils import timezone
 from channels.db import database_sync_to_async
 from courses.models import Course
 from .models import Message
+from django.utils.html import escape
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -13,47 +14,51 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.course_id = self.scope["url_route"]["kwargs"]["course_id"]
         self.room_group_name = f"chat_{self.course_id}"  # course id
         # join room group
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         # accept connection, might to reject if demand
         await self.accept()
 
     async def disconnect(self, close_code):
         # Leave room group
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data):
         """Вызывается только у пользователя, который отправил сообщение"""
         text_data_json = json.loads(text_data)  # json in dict
-        message = text_data_json["message"]  # exctract message
+        message = text_data_json["message"]  # extract message
         now = timezone.now()
         # self.send(text_data=json.dumps({"message": message}))  # сообщение в канал
         await self.channel_layer.group_send(  # Сообщение в группу
             self.room_group_name,
             {
                 "type": "chat_message",  # Invoke chat_message method *
-                "message": message,
-                "user": self.user.get_full_name(),
+                "message": await self.async_escape(message),  # escape for use in HTML
+                "user": self.user.username,
+                "user_full_name": await self.async_get_full_name(self.user),
                 "datetime": now.isoformat(),
-            }
+            },
         )
         # save message in db
-        course = await self.get_course(self.course_id)
-        save_message = await self.save_message(course, self.user, message)
-        # print(f"Сообщение '{self.save_message}' было сохранено.")
+        course = await Course.objects.aget(pk=self.course_id)  # async get course
+        # async create message
+        message = await Message.objects.acreate(course=course, user=self.user, content=message)
+        # print(f"Сообщение '{self.message}' было сохранено.")
 
-    @database_sync_to_async
-    def get_course(self, pk):
-        return Course.objects.get(pk=pk)
+    # @database_sync_to_async
+    # def get_course(self, pk):
+    #     return Course.objects.get(pk=pk)
 
-    @database_sync_to_async
-    def save_message(self, course, user, content):
-        Message.objects.create(course=course, user=user, content=content)
+    # @database_sync_to_async
+    # def save_message(self, course, user, content):
+    #     Message.objects.create(course=course, user=user, content=content)
+
+    async def async_get_full_name(self, user):
+        """Async return full name of user"""
+        return user.get_full_name()
+
+    async def async_escape(self, text):
+        """Returns the given text with ampersands, quotes and angle brackets encoded for use in HTML."""
+        return escape(text)
 
     async def chat_message(self, event):
         """Вызывается у всех пользователей включая, того кто отправил сообщение"""
---
+++ static/js/chat/course_chat_room.js
@@ -0,0 +1,81 @@
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("course_chat_room script successfully included");
+});
+
+const courseId = JSON.parse(document.getElementById("course-id").textContent);  // course.id from json format
+const requestUser = JSON.parse(document.getElementById("request-user").textContent); // requesUser from json format
+const input = document.getElementById("chat-message-input");  // message input
+const submitButton = document.getElementById("chat-message-submit");  // message button
+
+// focus input area
+input.focus();
+
+// Прослушка клавиши enter для отправки сообщения
+input.addEventListener("keypress", function(event) {
+    if (event.keyCode === 13) {  // ==="enter"
+      // cancel the default action, if needed
+      event.preventDefault();
+      // trigger click event on button
+      submitButton.click();
+    }
+});
+
+// Определение прослушки событий нажатия на кнопку отправки сообщения
+submitButton.addEventListener("click", function(event) {
+    const message = input.value;  // Извлекаем значение сообщения
+    if (message) {  // Если сообщение не пустое
+      // send message in JSON format on websocket
+      chatSocket.send(JSON.stringify({"message": message}));
+      // clear input
+      input.value = '';
+      input.focus();
+    }
+});
+
+let chatSocket = null;
+
+function connect(){
+    // Подключаем webSocket к страничке wss:// for prod; ws:// for local #}  // window.location.host - адрес-хост сайта
+    const url = "ws://" + window.location.host + "/ws/chat/room/" + courseId + "/";
+    chatSocket = new WebSocket(url);
+
+    chatSocket.onopen = function(e) {
+        console.log("Successfully connected to the WebSocket.");
+    }
+
+    chatSocket.onclose = function(event) {
+        console.error("WebSocket connection closed unexpectedly. Trying to reconnect in 2s...");
+        setTimeout(function(e) {
+            console.log("Reconnecting...");
+            connect();
+        }, 2000);
+    };
+
+    // Эта функция срабатывает когда получаем данные (сообщения) через ВебСокет
+    chatSocket.onmessage = function (event) {
+        const data = JSON.parse(event.data);
+        const chat = document.getElementById("chat");
+
+        const dateOptions = {hour: 'numeric', minute: 'numeric', hour12: true};
+        const datetime = new Date(data.datetime).toLocaleString(dateOptions);  <!-- ('en', dateOptions)  -->
+        const isMe = data.user === requestUser;
+        const source = isMe ? "me" : "other";
+        const name = isMe ? "Me" : data.user_full_name;
+
+        // Добавить сообщение в чат и заскроллить вниз
+        chat.innerHTML += '<div class="message ' + source + '">' + '<strong>' + name + '</strong>' + '<br><span class="date">' +
+                            datetime + '</span><br>' + data.message + '</div>';
+        chat.scrollTop = chat.scrollHeight;
+    };
+
+    chatSocket.onerror = function(err) {
+        console.log("WebSocket encountered an error: " + err.message);
+        console.log("Closing the socket.");
+        chatSocket.close();
+    }
+}
+
+connect();
+
+
+
