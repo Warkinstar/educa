270bee1c6967999568771ca6bb69ede5f1ffe6a9
Ramil <Warkinstar@mail.ru>
2023-05-10 12:16:13 +0600

    AppRegistryNotReady: Apps aren't loaded yet
    Исправлена ошибка при развертывании на fly django.core.exceptions.AppRegistryNotReady: Apps aren't loaded yet.
    В вашем случае, проблема заключалась в том, что в файле asgi.py вы пытались импортировать модуль chat.routing до загрузки приложений Django. Внутри этого модуля, вероятно, имелись зависимости от других модулей или приложений Django, которые ещё не были загружены, что вызвало исключение AppRegistryNotReady

--- educa/asgi.py
+++ educa/asgi.py
@@ -13,11 +13,11 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
-import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "educa.settings")
-
 django_asgi_app = get_asgi_application()
+
+import chat.routing
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
