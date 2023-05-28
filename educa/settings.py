ffaf4e193e2a2589a3392c184c46b8bae4569d94
Ramil <Warkinstar@mail.ru>
2023-05-28 17:27:33 +0600

    fly, settings env, pasww_reset_mess

--- educa/settings.py
+++ educa/settings.py
@@ -289,8 +289,8 @@ AUTHENTICATION_BACKENDS = [
 # Google smpt
 # EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = env.str("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = env.str("EMAIL_HOST_USER", default="-")
+EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD", default="-")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
